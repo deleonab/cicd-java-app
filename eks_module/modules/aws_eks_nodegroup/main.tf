f175a0119b5228a2d3c43e6617c7a5046611a07c
deleonab <delesodade@hotmail.com>
2023-06-06 20:05:52 +0100

    node group name updated

--- eks_module/modules/aws_eks_nodegroup/main.tf
+++ eks_module/modules/aws_eks_nodegroup/main.tf
@@ -21,7 +21,7 @@ resource "aws_eks_node_group" "nodes_general" {
     desired_size = 1
 
     # Maximum number of worker nodes.
-    max_size = 1
+    max_size = 2
 
     # Minimum number of worker nodes.
     min_size = 1
